e7eff9208364e76c6a8733beb243739d68e92e1c
Pedro Rafel <pedro.rafael.marinho@gmail.com>
2025-07-03 14:34:54 -0300

    Update

--- validacao_cruzada.r
+++ validacao_cruzada.r
@@ -26,3 +26,49 @@ validacao_cruzada <- function(x, k = 3L) {
 
 d <- validacao_cruzada(mtcars, k = 5)
 View(d)
+
+# Ajustando uma regressão polinomial
+
+gerando_dados <- function(n = 350L, ...) {
+  f <- function(x, sd = 0.5, ...) {
+    45 * tanh(x / 1.9 - 7) + 57 + rnorm(n, sd = sd)
+  }
+
+  tibble(x = runif(n = n, min = 0, max = 20)) |>
+    mutate(y = f(x, ...)) |>
+    relocate(y, .before = x)
+}
+
+tunagem <- function(cv, p_max = 25L) {
+  k <- max(cv$split)
+
+  regressao <- function(p, i) {
+    treino <- cv |>
+      dplyr::filter(split == i, info == "treino")
+    lm(y ~ poly(x, degree = p), data = treino)
+  }
+
+  avaliacao <- function(p) {
+    one_step <- function(i) {
+      r <- regressao(p, i)
+      validacao <- cv |>
+        dplyr::filter(split == i, info == "validacao")
+      pred <- predict(r, newdata = validacao)
+      mse <- mean((pred - validacao$y)^2)
+      tibble(p = p, split = i, mse = mse)
+    }
+    purrr::map_dfr(.x = 1L:k, .f = one_step)
+  }
+  resultados <- purrr::map_dfr(.x = 1L:p_max, .f = avaliacao)
+  resultados |>
+    group_by(p) |>
+    summarise(mse = mean(mse))
+}
+
+set.seed(123)
+dados <- gerando_dados(n = 150, mean = 0, sd = 5.5)
+
+cv_dados <- validacao_cruzada(dados, k = 10L)
+
+r <- tunagem(cv_dados, p_max = 25)
+print(r)
